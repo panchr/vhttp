05c484871b3d370be4a3cc88b6d23fa12469cbb9
Rushy Panchal <panchal.rushy@gmail.com>
2018-01-10 11:26:26 -0500

    Move http-mitm Dockerfile into mitm folder

--- bin/proxy.sh
+++ bin/proxy.sh
@@ -6,11 +6,12 @@
 # Description: Run an HTTP Proxy using mitmproxy.
 
 if command -v docker; then
-  docker build . -t http-mitm:latest
+  docker build mitm -f mitm/Dockerfile -t http-mitm:latest
   docker run --rm -p 8080:8080 http-mitm:latest
 else
   if command -v mitmdump; then
     echo "Please install mitmproxy and mitmdump: https://mitmproxy.org."
+    echo "Alternatively, Docker is also supported."
     exit 1
   fi
 
